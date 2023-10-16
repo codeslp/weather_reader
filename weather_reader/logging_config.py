b4fb6e918be7b753770fa280883bfa62b67d1b97
Brian Farish <brian.farish@gmail.com>
2023-10-16 16:39:54 -0500

    add logging

---
+++ weather_reader/logging_config.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+root_logger = logging.getLogger()
+root_logger.addHandler(file_handler)
