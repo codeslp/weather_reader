9f7d7902566951f1d571991ef9a925f6603a0551
Brian Farish <brian.farish@gmail.com>
2023-10-15 22:01:07 -0500

    added cities dict

---
+++ weather_reader/config.py
@@ -0,0 +1,22 @@
+cities = {
+    "Istanbul": {"country": "TR", "state": "34"},  # Istanbul Province
+    "London": {"country": "GB", "state": "ENG"},   # England
+    "Saint Petersburg": {"country": "RU", "state": "SPE"},  # St. Petersburg Federal City
+    "Berlin": {"country": "DE", "state": "BE"},    # Berlin State
+    "Madrid": {"country": "ES", "state": "M"},     # Community of Madrid
+    "Kyiv": {"country": "UA", "state": "32"},      # Kyiv city
+    "Rome": {"country": "IT", "state": "62"},      # Lazio Region
+    "Bucharest": {"country": "RO", "state": "B"},  # Bucharest
+    "Paris": {"country": "FR", "state": "75"},     # Île-de-France Region
+    "Minsk": {"country": "BY", "state": "HM"},     # Minsk Region
+    "Vienna": {"country": "AT", "state": "9"},     # Vienna State
+    "Warsaw": {"country": "PL", "state": "MZ"},    # Masovian Voivodeship
+    "Hamburg": {"country": "DE", "state": "HH"},   # Hamburg State
+    "Budapest": {"country": "HU", "state": "BU"},  # Budapest
+    "Belgrade": {"country": "RS", "state": "00"},  # Belgrade city
+    "Barcelona": {"country": "ES", "state": "CT"}, # Catalonia
+    "Munich": {"country": "DE", "state": "BY"},    # Bavaria State
+    "Kharkiv": {"country": "UA", "state": "63"},   # Kharkiv Oblast
+    "Milan": {"country": "IT", "state": "25"}      # Lombardy Region
+}
+
