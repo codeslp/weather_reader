13d7315f36a1cb06ab6f4135665ac17b76fbe2fc
Brian Farish <brian.farish@gmail.com>
2023-10-17 22:18:24 -0500

    formatting

--- weather_reader/logging_config.py
+++ weather_reader/logging_config.py
@@ -1,12 +1,14 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, 
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
-file_handler = logging.FileHandler('logs.log')
+file_handler = logging.FileHandler("logs.log")
 file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 
 root_logger = logging.getLogger()
