1de6aac94a23c0d3ca61ba481e2aa4b41bf82f50
Brian Farish <brian.farish@gmail.com>
2023-10-18 22:06:38 -0500

    fixed async call

--- weather_reader/main.py
+++ weather_reader/main.py
@@ -1,5 +1,6 @@
 from download_seq import download_many as download_readings_seq
 from download_concur import download_many as download_readings_concur
+from download_async import download_many as download_readings_async
 from validate_reading import validate_reading
 from download_common import (
     save_to_pq,
@@ -9,6 +10,7 @@ from download_common import (
     city_lat_lon,
 )
 import time
+
 
 def main(city_lat_lon, concur_type=None, max_concur_req=None):
     initial_report((concur_type, max_concur_req), city_lat_lon)
@@ -20,32 +22,39 @@ def main(city_lat_lon, concur_type=None, max_concur_req=None):
         )
         df = result[0]
         counter = result[1]
+        valid_readings_batch = validate_reading(df)
+        save_to_pq(valid_readings_batch)
+
     elif concur_type == "process":
-        if __name__ == '__main__':
+        if __name__ == "__main__":
             result = download_readings_concur(
                 SERVERS["WEATHER"], city_lat_lon, "process", max_concur_req
             )
         df = result[0]
         counter = result[1]
+        valid_readings_batch = validate_reading(df)
+        save_to_pq(valid_readings_batch)
+
     elif concur_type == "coroutine":
-        if __name__ == '__main__':
+        if __name__ == "__main__":
             result = download_readings_async(
-                SERVERS["WEATHER"], city_lat_lon, max_concur_req
+                SERVERS["WEATHER"], city_lat_lon, max_concur_req=len(city_lat_lon)
             )
         df = result[0]
         counter = result[1]
+
     else:
-        if __name__ == '__main__':
+        if __name__ == "__main__":
             result = download_readings_seq(SERVERS["WEATHER"], city_lat_lon)
         df = result[0]
         counter = result[1]
+        valid_readings_batch = validate_reading(df)
+        save_to_pq(valid_readings_batch)
 
-    valid_readings_batch = validate_reading(df)
-    save_to_pq(valid_readings_batch)
     final_report(counter, t0)
 
     return df
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     print(main(city_lat_lon, "thread"))
