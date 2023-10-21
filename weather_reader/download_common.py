dc6ea24adff43d5089b0b90f356ec10def5c7119
Brian Farish <brian.farish@gmail.com>
2023-10-20 22:04:07 -0500

    restructured lat_lon update fn

--- weather_reader/download_common.py
+++ weather_reader/download_common.py
@@ -1,26 +1,21 @@
-from dotenv import load_dotenv
-import os
-from urllib.parse import urlencode
-import httpx
-from dotenv import load_dotenv
-from datetime import datetime
 import logging
-import logging_config
-import pandas as pd
+import os
+import time
+from datetime import datetime
+from enum import Enum
+from pathlib import Path
+from collections import Counter
+
 import pyarrow as pa
 import pyarrow.parquet as pq
-from pathlib import Path
-from enum import Enum
+from dotenv import load_dotenv
 
-import time
-from collections import Counter
 
 PROJECT_DIR_PATH = Path(__file__).resolve().parents[1]
 DATA_DIR_PATH = PROJECT_DIR_PATH / "data"
 DATA_DIR_PATH.mkdir(parents=True, exist_ok=True)
 
 load_dotenv(PROJECT_DIR_PATH / ".env")
-
 API_KEY = os.getenv("API_KEY")
 
 SERVERS = {
@@ -30,109 +25,19 @@ SERVERS = {
 
 DownloadStatus = Enum("DownloadStatus", "OK NOT_FOUND ERROR")
 
-cities = {
-    "Istanbul": {"country": "TR"},
-    "London": {"country": "GB"},
-    "Saint Petersburg": {"country": "RU"},
-    "Berlin": {"country": "DE"},
-    "Madrid": {"country": "ES"},
-    "Kyiv": {"country": "UA"},
-    "Rome": {"country": "IT"},
-    "Bucharest": {"country": "RO"},
-    "Paris": {"country": "FR"},
-    "Minsk": {"country": "BY"},
-    "Vienna": {"country": "AT"},
-    "Warsaw": {"country": "PL"},
-    "Hamburg": {"country": "DE"},
-    "Budapest": {"country": "HU"},
-    "Belgrade": {"country": "RS"},
-    "Barcelona": {"country": "ES"},
-    "Munich": {"country": "DE"},
-    "Kharkiv": {"country": "UA"},
-    "Milan": {"country": "IT"},
-}
-
-city_lat_lon = {
-    "Istanbul": {"country": "TR", "lat": 41.0091982, "lon": 28.9662187},
-    "London": {"country": "GB", "lat": 51.5073219, "lon": -0.1276474},
-    "Saint Petersburg": {"country": "RU", "lat": 59.938732, "lon": 30.316229},
-    "Berlin": {"country": "DE", "lat": 52.5170365, "lon": 13.3888599},
-    "Madrid": {"country": "ES", "lat": 40.4167047, "lon": -3.7035825},
-    "Kyiv": {"country": "UA", "lat": 50.4500336, "lon": 30.5241361},
-    "Rome": {"country": "IT", "lat": 41.8933203, "lon": 12.4829321},
-    "Bucharest": {"country": "RO", "lat": 44.4361414, "lon": 26.1027202},
-    "Paris": {"country": "FR", "lat": 48.8588897, "lon": 2.3200410217200766},
-    "Minsk": {"country": "BY", "lat": 53.9024716, "lon": 27.5618225},
-    "Vienna": {"country": "AT", "lat": 48.2083537, "lon": 16.3725042},
-    "Warsaw": {"country": "PL", "lat": 52.2319581, "lon": 21.0067249},
-    "Hamburg": {"country": "DE", "lat": 53.550341, "lon": 10.000654},
-    "Budapest": {"country": "HU", "lat": 47.4979937, "lon": 19.0403594},
-    "Belgrade": {"country": "RS", "lat": 44.8178131, "lon": 20.4568974},
-    "Barcelona": {"country": "ES", "lat": 41.3828939, "lon": 2.1774322},
-    "Munich": {"country": "DE", "lat": 48.1371079, "lon": 11.5753822},
-    "Kharkiv": {"country": "UA", "lat": 49.9923181, "lon": 36.2310146},
-    "Milan": {"country": "IT", "lat": 45.4641943, "lon": 9.1896346},
-}
 
 
-def update_city_lat_long(cities=cities, city_lat_lon=None):
+def initial_report(actual_args, city_lat_lon) -> None:
     """
-    Update or retrieve latitude and longitude coordinates for a list of cities.
-
-    This function either updates the latitude and longitude coordinates of the provided cities
-    or retrieves them if not already available in the `city_lat_lon` dictionary. If the `city_lat_lon`
-    dictionary does not contain coordinates for all cities, it will be updated.
+    Log the initial report for weather data retrieval.
 
     Args:
-        cities (dict): A dictionary containing city information, including country and state (if applicable).
-        city_lat_lon (dict, optional): A dictionary containing city information with 'lat' and 'lon' coordinates.
+        actual_args (tuple): A tuple containing concurrency type and max concurrency.
+        city_lat_lon (dict): A dictionary containing city information with latitude and longitude.
 
     Returns:
-        dict: A dictionary containing city information with 'lat' and 'lon' coordinates.
+        None
     """
-    if (city_lat_lon is None) or (cities.keys() != city_lat_lon.keys()):
-        logger = logging.getLogger(__name__)
-        logging.info("Updating city latitude and longitude coordinates.")
-        logging.info("Starting update at: " + str(datetime.now()))
-        city_lat_lon = get_lat_long(cities)
-        logging.info("Finished update at: " + str(datetime.now()))
-    return city_lat_lon
-
-
-def get_lat_long(cities=cities) -> dict:
-    """
-    Retrieves latitude and longitude coordinates for a list of cities.
-
-    Args:
-        cities (dict): A dictionary containing city information, including country and state (if applicable).
-
-    Returns:
-        dict: A dictionary containing city information with added 'lat' and 'lon' coordinates.
-    """
-
-    base_url = SERVERS["LATLONG"]
-
-    for city, co_st in cities.items():
-        country_code = co_st["country"]
-
-        if country_code == "US":
-            q_value = f"{city},{co_st['state']},{country_code}"
-        else:
-            q_value = f"{city},{country_code}"
-
-        params = {"q": q_value, "limit": 1, "appid": API_KEY}
-
-        url = base_url + urlencode(params)
-        response = httpx.get(url)
-        response.raise_for_status()
-        data = response.json()
-        cities[city]["lat"] = data[0]["lat"]
-        cities[city]["lon"] = data[0]["lon"]
-
-    return cities
-
-
-def initial_report(actual_args, city_lat_lon):
     logger = logging.getLogger(__name__)
     logger.info(f"Getting weather for: {list(city_lat_lon.keys())}")
     logger.info(f"Concurrency type: {actual_args[0]}")
@@ -140,7 +45,7 @@ def initial_report(actual_args, city_lat_lon):
     logger.info(f"Time started: {datetime.now()}")
 
 
-def save_to_pq(df):
+def save_to_pq(df) -> None:
     """
     Save a DataFrame to a Parquet file locally.
 
@@ -171,9 +76,19 @@ def save_to_pq(df):
 
 
 def final_report(counter: Counter[DownloadStatus], start_time: datetime) -> None:
+    """
+    Log the final report for weather data retrieval.
+
+    Args:
+        counter (collections.Counter): A Counter object with download status counts.
+        start_time (datetime.datetime): The start time of the data retrieval process.
+
+    Returns:
+        None
+    """
     elapsed = time.perf_counter() - start_time
     plural = "s" if counter[DownloadStatus.OK] != 1 else ""
-    logging.info(f"{counter[DownloadStatus.OK]:3} readings{plural} downloaded.")
+    logging.info(f"{counter[DownloadStatus.OK]:3} reading{plural} downloaded.")
     if counter[DownloadStatus.NOT_FOUND]:
         logging.error(f"{counter[DownloadStatus.NOT_FOUND]:3} not found.")
     if counter[DownloadStatus.ERROR]:
--- weather_reader/main.py
+++ weather_reader/main.py
@@ -1,18 +1,136 @@
-from download_seq import download_many as download_readings_seq
-from download_concur import download_many as download_readings_concur
-from download_async import download_many as download_readings_async
-from validate_reading import validate_reading
+import logging
+import time
+from datetime import datetime
+from urllib.parse import urlencode
+from enum import Enum
+import httpx
+import shelve
+
 from download_common import (
     save_to_pq,
     initial_report,
     final_report,
     SERVERS,
-    city_lat_lon,
+    API_KEY,
 )
-import time
+from download_async import download_many as download_readings_async
+from download_concur import download_many as download_readings_concur
+from download_seq import download_many as download_readings_seq
+from validate_reading import validate_reading
 
 
-def main(city_lat_lon, concur_type=None, max_concur_req=None):
+
+
+cities = {
+    "Istanbul": {"country": "TR"},
+    "London": {"country": "GB"},
+    "Saint Petersburg": {"country": "RU"},
+    "Berlin": {"country": "DE"},
+    "Madrid": {"country": "ES"},
+    "Kyiv": {"country": "UA"},
+    "Rome": {"country": "IT"},
+    "Bucharest": {"country": "RO"},
+    "Paris": {"country": "FR"},
+    "Minsk": {"country": "BY"},
+    "Vienna": {"country": "AT"},
+    "Warsaw": {"country": "PL"},
+    "Hamburg": {"country": "DE"},
+    "Budapest": {"country": "HU"},
+    "Belgrade": {"country": "RS"},
+    "Barcelona": {"country": "ES"},
+    "Munich": {"country": "DE"},
+    "Kharkiv": {"country": "UA"},
+    "Milan": {"country": "IT"},
+}
+
+
+
+def update_city_lat_lon(cities: dict) -> dict:
+    """
+    Update or retrieve latitude and longitude coordinates for a list of cities.
+
+    This function either updates the latitude and longitude coordinates of the provided cities
+    or retrieves them if not already available in the `cities` dictionary. If the coordinates
+    are retrieved or updated, they are stored in a dictionary with city names as keys and 
+    dictionaries containing 'lat' and 'lon' coordinates as values.
+
+    Args:
+        cities (dict): A dictionary containing city information, including country and state (if applicable).
+
+    Returns:
+        dict: A dictionary containing city information with 'lat' and 'lon' coordinates.
+    """
+
+    with shelve.open("city_lat_lon") as db:
+        if db:
+            city_lat_lon = db["city_lat_lon"]
+        else:
+            city_lat_lon = None
+
+    if (city_lat_lon is None) or (cities.keys() != city_lat_lon.keys()):
+        logging.info("Updating city latitude and longitude coordinates.")
+        logging.info("Starting update at: " + str(datetime.now()))
+        city_lat_lon = get_lat_lon(cities)
+        logging.info("Finished update at: " + str(datetime.now()))
+        with shelve.open("city_lat_lon") as db:
+            db["city_lat_lon"] = city_lat_lon
+    
+    return city_lat_lon
+
+
+def get_lat_lon(cities: dict) -> dict:
+    """
+    Retrieves latitude and longitude coordinates for a list of cities.
+
+    Args:
+        cities (dict): A dictionary containing city information, including country and state (if applicable).
+
+    Returns:
+        dict: A dictionary containing city information with added 'lat' and 'lon' coordinates.
+    """
+
+    base_url = SERVERS["LATLONG"]
+
+    for city, co_st in cities.items():
+        country_code = co_st["country"]
+
+        if country_code == "US":
+            q_value = f"{city},{co_st['state']},{country_code}"
+        else:
+            q_value = f"{city},{country_code}"
+
+        params = {"q": q_value, "limit": 1, "appid": API_KEY}
+
+        url = base_url + urlencode(params)
+        response = httpx.get(url)
+        response.raise_for_status()
+        data = response.json()
+        cities[city]["lat"] = data[0]["lat"]
+        cities[city]["lon"] = data[0]["lon"]
+
+    return cities
+
+
+def main(concur_type=None, max_concur_req=None):
+    """
+    Main function to orchestrate the data download and processing workflow.
+
+    This function retrieves latitude and longitude coordinates for a list of cities, 
+    downloads weather data based on the specified concurrency type, and saves the 
+    valid data to a Parquet file. It also generates and logs initial and final reports 
+    including concurrency type, max concurrency, and elapsed time.
+
+    Args:
+        concur_type (str, optional): The concurrency type to use for downloading data 
+            (e.g., 'thread', 'process', 'coroutine'). Default is None.
+        max_concur_req (int, optional): The maximum number of concurrent requests to 
+            make during data download. Default is None.
+
+    Returns:
+        None
+    """
+
+    city_lat_lon = update_city_lat_lon(cities)
     initial_report((concur_type, max_concur_req), city_lat_lon)
     t0 = time.perf_counter()
 
@@ -57,4 +175,4 @@ def main(city_lat_lon, concur_type=None, max_concur_req=None):
 
 
 if __name__ == "__main__":
-    print(main(city_lat_lon, "thread"))
+    print(main(concur_type=None))
