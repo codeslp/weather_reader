d63aa3f89e4ee3326e684fff4977dca36a02690f
Brian Farish <brian.farish@gmail.com>
2023-10-29 22:36:25 -0500

    fix logging implement json config file for cities

--- weather_reader/main.py
+++ weather_reader/main.py
@@ -1,11 +1,11 @@
-
 import logging
+from logging_config import configure_logger
 import time
 from datetime import datetime
+import json
 from urllib.parse import urlencode
-from enum import Enum
 import httpx
-import shelve
+from pathlib import Path
 
 from download_common import (
     save_to_pq,
@@ -20,62 +20,62 @@ from download_concur import download_many as download_readings_concur
 from download_seq import download_many as download_readings_seq
 from validate_reading import validate_reading
 
+configure_logger()
 
-cities = {
-    "Istanbul": {"country": "TR"},
-    "London": {"country": "GB"},
-    "Saint Petersburg": {"country": "RU"},
-    "Berlin": {"country": "DE"},
-    "Madrid": {"country": "ES"},
-    "Kyiv": {"country": "UA"},
-    "Rome": {"country": "IT"},
-    "Bucharest": {"country": "RO"},
-    "Paris": {"country": "FR"},
-    "Minsk": {"country": "BY"},
-    "Vienna": {"country": "AT"},
-    "Warsaw": {"country": "PL"},
-    "Hamburg": {"country": "DE"},
-    "Budapest": {"country": "HU"},
-    "Belgrade": {"country": "RS"},
-    "Barcelona": {"country": "ES"},
-    "Munich": {"country": "DE"},
-    "Kharkiv": {"country": "UA"},
-    "Milan": {"country": "IT"},
-    "Prague": {"country": "CZ"},
-}
+PROJECT_DIR_PATH = Path(__file__).resolve().parents[1]
+CITIES_CONFIG_PATH = PROJECT_DIR_PATH / "weather_reader" / "cities.json"
 
 
-def update_city_lat_lon(cities: dict) -> dict:
+def update_city_lat_lon() -> dict:
     """
-    Update or retrieve latitude and longitude coordinates for a list of cities.
+    Update or retrieve latitude and longitude coordinates for a list of cities from a JSON file.
 
-    This function either updates the latitude and longitude coordinates of the provided cities
-    or retrieves them if not already available in the `cities` dictionary. If the coordinates
-    are retrieved or updated, they are stored in a dictionary with city names as keys and
-    dictionaries containing 'lat' and 'lon' coordinates as values.
-
-    Args:
-        cities (dict): A dictionary containing city information, including country and state (if applicable).
+    This function checks if the latitude and longitude coordinates of the cities in the JSON file are up to date.
+    If not, it updates the coordinates and writes them back to the file.
 
     Returns:
-        dict: A dictionary containing city information with 'lat' and 'lon' coordinates.
+        dict: A dictionary containing city information with updated 'lat' and 'lon' coordinates.
     """
+    try:
+        with open(CITIES_CONFIG_PATH, "r") as file:
+            data = json.load(file)
+            cities = data.get("cities", {})
+            city_lat_lon = data.get("city_lat_lon", {})
+            cached_city_lat_lon = data.get("cached_city_lat_lon", {})
 
-    with shelve.open("city_lat_lon") as db:
-        if db:
-            city_lat_lon = db["city_lat_lon"]
-        else:
-            city_lat_lon = None
+        cities_key_set = set(cities.keys())
+        if (cities_key_set != set(city_lat_lon.keys())) and cities_key_set.issubset(
+            set(cached_city_lat_lon.keys())
+        ):
+            logging.info(
+                "Attempting to update city latitude and longitude coordinates from cache."
+            )
+            logging.info(f"Starting update at: {datetime.now()}")
+            keys_to_update = cities_key_set - set(city_lat_lon.keys())
+            for key in keys_to_update:
+                city_lat_lon[key] = cached_city_lat_lon[key]
+            data["city_lat_lon"] = city_lat_lon
+            with open(CITIES_CONFIG_PATH, "w") as file:
+                json.dump(data, file, indent=4)
+            logging.info(f"Finished update at: {datetime.now()}")
 
-    if (city_lat_lon is None) or (cities.keys() != city_lat_lon.keys()):
-        logging.info("Updating city latitude and longitude coordinates.")
-        logging.info("Starting update at: " + str(datetime.now()))
-        city_lat_lon = get_lat_lon(cities)
-        logging.info("Finished update at: " + str(datetime.now()))
-        with shelve.open("city_lat_lon") as db:
-            db["city_lat_lon"] = city_lat_lon
+        elif cities_key_set != set(city_lat_lon.keys()):
+            logging.info("Updating city latitude and longitude coordinates.")
+            logging.info(f"Starting update at: {datetime.now()}")
+            updated_city_lat_lon = get_lat_lon(cities)
+            data["city_lat_lon"] = updated_city_lat_lon
+            with open(CITIES_CONFIG_PATH, "w") as file:
+                json.dump(data, file, indent=4)
+            logging.info(f"Finished update at: {datetime.now()}")
 
-    return city_lat_lon
+
+
+        return city_lat_lon
+
+    except FileNotFoundError:
+        logging.error(f"Could not find the file at {CITIES_CONFIG_PATH}")
+    except KeyError as e:
+        logging.error(f"Unexpected JSON format: Missing key {e}")
 
 
 def get_lat_lon(cities: dict) -> dict:
@@ -130,7 +130,7 @@ def main(concur_type=None, max_concur_req=None):
         None
     """
 
-    city_lat_lon = update_city_lat_lon(cities)
+    city_lat_lon = update_city_lat_lon()
     initial_report((concur_type, max_concur_req), city_lat_lon)
     t0 = time.perf_counter()
 
@@ -164,4 +164,4 @@ def main(concur_type=None, max_concur_req=None):
 
 
 if __name__ == "__main__":
-    print(main(concur_type="coroutine"))
+    print(main(concur_type='coroutine'))
